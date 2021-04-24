9fc0fd242d1f3be3e0990a200868f74368e8a6f7
codydeatherage <cdeatherage14@gmail.com>
2021-04-23 23:47:14 -0700

    simple api response

---
+++ app.js
@@ -0,0 +1,61 @@
+/* import {createRequire} from 'module';
+const require = createRequire(import.meta.url); */
+const auth = require('./auth.json');
+const axios = require('axios');
+/* let allChamps = [];
+
+const apiKey = "RGAPI-cbb4716d-90f4-4cb9-b571-98d8b6309c61";
+
+axios.post('https://na1.api.riotgames.com/lol/platform/v3/champion-rotations', 
+            { headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Origin": "https://developer.riotgames.com",
+                "X-Riot-Token": {apiKey}
+                }
+    }).then( (response) => {
+        console.log('promise done');
+        const {data} = response.data;
+        for(let champ in data){
+            console.log(champ);
+            allChamps.push(champ);
+        }
+    }).catch( (e) => {
+        console.error(e);
+    })
+
+  
+
+
+
+
+for(let i = 0; i < allChamps.length; i++){
+    console.log(allChamps);
+}
+
+
+
+ */
+
+async function getAllChampNames(){
+    const allChamps = [];
+    try{
+      const prom = axios.get('http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion.json',
+                                  {params: {
+                                      ID: 'RGAPI-cbb4716d-90f4-4cb9-b571-98d8b6309c61'
+                                      }
+                                  });
+      const response = await prom;
+      const {data} = response.data;
+      for(let champ in data){
+          console.log(champ);
+          allChamps.push(champ);
+      }
+     
+      return allChamps;
+    }catch(err){console.error(err)}
+}
+
+    const allChamps = getAllChampNames();
+    console.log(allChamps);
