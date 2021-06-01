2d197de682316499da57e5e5bc878555ff4e23ba
codydeatherage <cdeatherage14@gmail.com>
2021-05-31 23:06:53 -0700

    plan for whole data collection/processing in place now

--- analyze-winrates.js
+++ analyze-winrates.js
@@ -8,19 +8,19 @@ const MatchDataDB = require('./MatchDataDB')
 const matchIDQuery = require('./MatchIDQuery');
 const dbUrl = 'mongodb://localhost/LoLWinrates';
 let allMatches = [];
-const analyzeMatches = async (callback) =>{
+const analyzeMatches = async (callback) => {
     MongoClient.connect(dbUrl, { useUnifiedTopology: true }, async (err, client) => {
         const db = client.db('LoLWinrates');
         let arr = await db.collection('challenger-match-data').find({ gameId: { $exists: true } }).toArray();
-    
+
         console.log('done');
         allMatches = arr;
         callback();
         client.close();
-        
+
     })
 }
-const test = async () =>{
+const test = async () => {
     let cb = () => {
         console.log(allMatches[0].teams[1].win);
     }
@@ -41,8 +41,30 @@ const test = async () =>{
     matchups: [{champName: , gamesPlayed: , gamesWon: , gamesLost: }, ....]
 }
 */
-for(let ch in champs.champions){
-    console.log(ch);
-}
+
+MongoClient.connect(dbUrl, { useUnifiedTopology: true }, async (err, client) => {
+    const db = client.db('LoLWinrates');
+    /* let arr = await db.collection('champion-data'); */
+    for (let ch in champs.champions) {
+        await db.collection('champion-data').insertOne(
+            {
+                name: ch,
+                gamesPicked: 0,
+                gamesBanned: 0,
+                gamesOnBlue: 0,
+                gamesOnRed: 0,
+                gamesWonOnRed: 0,
+                gamesWonOnBlue: 0,
+                gamesWon: 0,
+                gamesLost: 0,
+                matchups: []
+            }
+        )
+    }
+    console.log('done');
+    client.close();
+
+})
+
 
 
